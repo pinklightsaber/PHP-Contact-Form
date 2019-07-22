06422e009db0a409c76d62035d6c10d4ded1c8fd
pinklightsaber <ctorres@idemar.cl>
2019-07-22 15:36:31 -0400

    mi correo

--- contactform.php
+++ contactform.php
@@ -6,7 +6,7 @@
  	$mailFrom = $_POST['mail'];
  	$message = $_POST['message'];
 
- 	$mailTo = "smudronja.sm@gmail.com";
+ 	$mailTo = "ctorres@idemar.cl";
  	$headers = "From: ".$mailFrom;
  	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
